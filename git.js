b2730b5df200158d56a37332531b5b74a3cff245
Plehanov Fedor <Mike3386@users.noreply.github.com>
2019-07-16 09:54:32 +0300

    better then before

--- git.js
+++ git.js
@@ -6,44 +6,47 @@ module.exports.GetUserRepositories = GetUserRepositories;
 module.exports.GetRepositoriesByLine = GetRepositoriesByLine;
 module.exports.GetCommitsForRepositorie = GetCommitsForRepositorie;
 
-function GetUserRepositories(user){
-    return new Promise((resolve, reject)=>{
-        if(user != null){
-            Needle.getAsync("https://api.github.com/users/" + user + "/repos?per_page=10").
-            then((data)=>{
-                if(data.body.message == "Not Found") resolve([]);
-                else resolve(data.body.map(function(elem){
-                        return {id:elem.id, name:elem.name, url:elem.url};
-                    }));
-                
-            }).catch(reject);
-        } else resolve([]);
-    })
+function GetUserRepositories(user) {
+  if (!user) return Promise.resolve([]);
+
+  return Needle.getAsync(`https://api.github.com/users/${user}/repos?per_page=10`)
+  .then(data => {
+    if (data.body.message === "Not Found") return [];
+
+    return data.body.map(elem => ({
+      id: elem.id,
+      name: elem.name,
+      url: elem.url
+    }));
+  });
 }
 
-function GetRepositoriesByLine(line){
-    return new Promise((resolve, reject)=>{
-        if(line != null){
-            Needle.getAsync("https://api.github.com/search/repositories?q=" + line + "&per_page=10").
-            then((data)=>{
-                resolve(data.body.items.map(function(elem){
-                    return {id:elem.id, name:elem.name, owner:elem.owner.login, url:elem.url};
-                }));
-            }).catch(reject);
-        } else resolve([]);
-    })
+function GetRepositoriesByLine(line) {
+  if (!line) return Promise.resolve([]);
+
+  return Needle.getAsync(`https://api.github.com/search/repositories?q=${line}&per_page=10`)
+  .then(data =>
+    data.body.items.map(elem => ({
+      id:elem.id,
+      name:elem.name,
+      owner:elem.owner.login,
+      url:elem.url
+    }));
+  )
 }
 
-function GetCommitsForRepositorie(repos){
-    return new Promise((resolve, reject)=>{
-        if(repos != null && repos.owner != null && repos.name != null){
-            Needle.getAsync("https://api.github.com/repos/"+ repos.owner + "/" + repos.name +"/commits?per_page=10").
-            then((data)=>{ 
-                if(data.body.message == "Not Found") resolve([]);
-                else resolve(data.body.map(function(elem){
-                        return {sha:elem.sha, author:elem.author.login, message:elem.commit.message, date:elem.commit.committer.date}
-                    }));
-            }).catch(reject);
-        } else resolve([]);
-    })
+function GetCommitsForRepositorie(repos) {
+  if (!repos && !repos.owner && !repos.name) return Promise.resolve([]);
+
+  return Needle.getAsync(`https://api.github.com/repos/${repos.owner}/${repos.name}/commits?per_page=10`)
+  .then(data => { 
+      if (data.body.message === "Not Found") return [];
+      
+      return data.body.map(elem => ({
+        sha:elem.sha,
+        author:elem.author.login,
+        message:elem.commit.message,
+        date:elem.commit.committer.date
+      });
+  });
 }
